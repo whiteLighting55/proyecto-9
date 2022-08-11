f96a7530d8c4c06f8e87e8ab41c0945249737cc2
whiteLighting55 <111091699+whiteLighting55@users.noreply.github.com>
2022-08-11 17:14:10 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var box;
+
+
+function setup() {
+  createCanvas(400,400); 
+  box = createSprite(200,200,45,45);
+}
+
+function draw() {
+  background("limegreen");
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x + 5;
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x - 5;
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y = box.position.y - 5;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y = box.position.y + 5;
+  }
+  drawSprites();
+}
+
